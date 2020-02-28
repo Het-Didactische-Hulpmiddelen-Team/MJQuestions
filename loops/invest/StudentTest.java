a216705c859c1c3fe7cd73699524c8ba54d6940c
lem-onade <taelemansst@gmail.com>
2020-02-28 11:26:49 +0100

    unused imports

--- loops/invest/StudentTest.java
+++ loops/invest/StudentTest.java
@@ -2,8 +2,6 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
-import java.sql.PreparedStatement;
-
 import static org.junit.Assert.assertEquals;
 
 public class StudentTest {
