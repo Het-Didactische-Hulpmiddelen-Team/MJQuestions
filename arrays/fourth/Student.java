289877a0f20aaa08cbff5b9fe64a0d65de92f216
arnevandoorslaer <arne.vandoorslaer@student.ucll.be>
2020-02-28 11:02:40 +0100

    arrays

---
+++ arrays/fourth/Student.java
@@ -0,0 +1,4 @@
+public class Student {
+    // Schrijf een functie fourth(int[] xs) die gegeven een array xs het vierde element ervan teruggeeft.
+    // Je mag ervan uitgaan dat xs altijd minstens vier elementen bevat.
+}
