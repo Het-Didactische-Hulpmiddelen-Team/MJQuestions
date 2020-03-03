e974e2d379c1bcd8cc2a0260ffd30ec346afc71f
lemonade <32491056+lem-onade@users.noreply.github.com>
2020-03-03 18:31:17 +0100

    hoofdletter

--- 01-booleans/isPositive/Student.java
+++ 01-booleans/isPositive/Student.java
@@ -1,4 +1,4 @@
-class student {
+class Student {
   // Implement the function 'static boolean isPositive(int)'
   // returns true if the integer is positive (0 or greater)
   // returns false if not
