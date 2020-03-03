19e7a0d6c9f7d8423be4ad701baf7fdfa8120749
BelgianNoise <king.arthur360@gmail.com>
2020-03-03 09:04:10 +0100

    waarom veranderen die bestandsnamen niet?

--- 01-booleans/inInterval/studentTest.java
+++ 01-booleans/inInterval/studentTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class studentTest {
+  
   @Test
   public void inIntervalReturnsTrueIfBetween(){
     assertTrue(student.inInterval(5, 0 , 10));
