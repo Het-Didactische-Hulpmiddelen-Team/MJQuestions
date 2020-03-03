951a331301cf692c16d11ded42fbeb7f23556ad5
lemonade <32491056+lem-onade@users.noreply.github.com>
2020-03-03 18:30:46 +0100

    hoofdletter

--- 01-booleans/inInterval/Student.java
+++ 01-booleans/inInterval/Student.java
@@ -1,4 +1,4 @@
-class student {
+class Student {
   // Implement the function 'static boolean inInterval(x, y, z)' that
   // returns true if x is higher than or the same as y and lower than or the same as z
   // returns false if not
