761f6a9af0cab494b95ac9cef949ae7ad17a0141
lem-onade <taelemansst@gmail.com>
2020-03-04 17:16:42 +0100

    typo fixed @arnevandoorslaer

--- 03-arrays/last/StudentTest.java
+++ 03-arrays/last/StudentTest.java
@@ -22,11 +22,11 @@ public class StudentTest {
 
     @Test
     public void last() {
-        assertEquals(4, s.fourth(new int[]{5,1,2,12,4}));
-        assertEquals(5, s.fourth(new int[]{0,1,2,9,4,5}));
-        assertEquals(7, s.fourth(new int[]{8,1,2,7}));
-        assertEquals(1, s.fourth(new int[]{9,1,2,4,4,1}));
-        assertEquals(1, s.fourth(new int[]{1,1,1,1,1,1}));
+        assertEquals(4, s.last(new int[]{5,1,2,12,4}));
+        assertEquals(5, s.last(new int[]{0,1,2,9,4,5}));
+        assertEquals(7, s.last(new int[]{8,1,2,7}));
+        assertEquals(1, s.last(new int[]{9,1,2,4,4,1}));
+        assertEquals(1, s.last(new int[]{1,1,1,1,1,1}));
     }
 
 }
