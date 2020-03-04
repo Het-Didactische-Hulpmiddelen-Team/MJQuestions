db4ccfecc6ea72c97f6b01e85c3733447a8d7e06
lemonade <32491056+lem-onade@users.noreply.github.com>
2020-03-04 16:00:27 +0100

    Update StudentTest.java

--- 01-booleans/inInterval/StudentTest.java
+++ 01-booleans/inInterval/StudentTest.java
@@ -2,7 +2,7 @@ import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class StudentTest {
   
