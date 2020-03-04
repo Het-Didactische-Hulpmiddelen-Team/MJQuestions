04996c585209de85958ea65feb086a6189880449
lemonade <32491056+lem-onade@users.noreply.github.com>
2020-03-04 16:00:45 +0100

    Update StudentTest.java

--- 01-booleans/isPositive/StudentTest.java
+++ 01-booleans/isPositive/StudentTest.java
@@ -1,7 +1,22 @@
+import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class StudentTest {
+  
+  private Student s;
+
+  @Before
+  public void setUp(){
+      s = new Student();
+  }
+
+  @After
+  public void tearDown(){
+      s = null;
+  }
   
   @Test
   public void isPositiveReturnsTrueIfPositive(){
