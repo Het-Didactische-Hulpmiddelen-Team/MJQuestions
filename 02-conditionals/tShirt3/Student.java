9df48a9c5b0c803ea93119f8de135a6e0ddd09c5
BelgianNoise <king.arthur360@gmail.com>
2020-03-04 17:01:25 +0100

    fix

--- 02-conditionals/tShirt3/Student.java
+++ 02-conditionals/tShirt3/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+class Student {
     // Je wil t-shirts laten drukken.
     // Per t-shirt kost het €10.
     // Als je er meer dan 100 bestelt, krijg je korting en kost het slechts €9 per t-shirt.
--- 03-arrays/last/Student.java
+++ 03-arrays/last/Student.java
@@ -1,4 +1,4 @@
-public class Student {
+class Student {
     // Schrijf een functie last(int[] xs) die gegeven een array xs het laatste element ervan teruggeeft.
     // Je mag ervan uitgaan dat xs minstens één element bevat.
 }
